edfa56a9d6886610fab9180d1f050ea1107b8d17
BHargreaves <2111407H@student.gla.ac.uk>
2018-03-06 17:10:51 +0000

    Views started -EO'H

--- higherguidanceforum/views.py
+++ higherguidanceforum/views.py
@@ -4,18 +4,35 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
+    context_dict = {}
     return HttpResponse("This will be the home page")
+    #return render(request, 'higherguidanceforum/home.html', context=context_dict)
 
 def about(request):
-    return HttpResponse("This will be the about page")
+    context_dict = {'boldmessage': "Here is the about page"}
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+    return render(request, 'scottishhigherguidanceforum/about.html', context=context_dict)
 
 def contact_us(request):
     return HttpResponse("This will be the contact us page")
 
 def subject_index(request):
-    return HttpResponse("This will be the subject index page")
+    return HttpResponse("This will be the index page")
 
-def show_subject(request):
+def show_subject(request, subject_name_slug):
+    context_dict = {}
+
+    try:
+        subject = Subject.objects.get(slug=category_name_slug)
+        links = Links.objects.filter(category=category)
+        context_dict['links'] = links
+        context_dict['subject'] = subject
+
+    except Subject.DoesNotExist:
+        context_dict['subject'] = None
+        context_dict['links'] = None
+    return render(request, 'scottishhigherguidanceforum/subject.html', context_dict)
     return HttpResponse("This will be a specific subject page")
 
 def show_resources(request):
@@ -38,7 +55,7 @@ def submit_answer(request):
 
 def register(request):
     registered = False
-    if request.method == 'POST'
+    if request.method == 'POST':
         user_form = UserForm(data=request.POST)
         profile_form = UserProfileForm(data=request.POST)
 
@@ -57,20 +74,48 @@ def register(request):
         else:
             printer(user_form.errors, profile_Form.errors)
 
-    else
-    return HttpResponse("This will be where you can register")
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileForm()
+
+    return render(request,
+        'scottishhigherguidanceforum/register.html',
+        {'user_form': user_form,
+        'profile_Form': profile_form,
+        'registered': register})
+
+
 
 def user_login(request):
-    return HttpResponse("This is the login form")
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+            else:
+                return HttpResponse("Your Scottish Higher Guidance Forum is disabled.")
+        else:
+            print("Invalid login details: {0}, {1}".format(username, password) )
+            return HttpResponse("Invalid login details supplied.")
+
+    else:
+        return render(request, 'scottishhigherguidanceforum/login.html', {})
+
 
 def my_account(request):
     return HttpResponse("This is the user account")
 
 def my_submissions(request):
-    return HttpResponse("This is the user's submission history")
+    return HttpResponse("This is the user submissions")
 
 def user_logout(request):
-    return HttpResponse("The is the logout screen")
+    logout(request)
+    return HttpResponseRedirect(reverse('index'))
+
 
 def user_list(request):
     return HttpResponse("This is the list of users")
