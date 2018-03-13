e9d7a7c300cfc79cc6bbc70f226785f147db5b3e
Ross Angus <2244073a@student.gla.ac.uk>
2018-03-13 12:28:00 +0000

    Ross Angus, changed views

---
+++ scottishhighersguidanceforum/higherguidanceforum/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from higherguidanceforum.models import Subject, Link, UserProfile
+# Register your models here.
+
+class ResourceAdmin(admin.ModelAdmin):
+    list_display = ('title','subject','url')
+
+class SubjectAdmin(admin.ModelAdmin):
+    prepopulate_fields = {'slug':('name','views','likes')}
+
+# Register your models here.
+admin.site.register(Subject, SubjectAdmin)
+admin.site.register(Link, ResourceAdmin)
+admin.site.register(UserProfile)
